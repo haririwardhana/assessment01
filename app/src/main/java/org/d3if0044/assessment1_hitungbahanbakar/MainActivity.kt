dd1b7207e7734114e8f8c0af912419393cef0394
haririwardhana <haririwardhana@student.telkomuniversity.ac.id>
2022-03-29 10:19:10 +0700

    Pembuatan sanity check dan validasi

--- app/src/main/java/org/d3if0044/assessment1_hitungbahanbakar/MainActivity.kt
+++ app/src/main/java/org/d3if0044/assessment1_hitungbahanbakar/MainActivity.kt
@@ -2,6 +2,8 @@ package org.d3if0044.assessment1_hitungbahanbakar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if0044.assessment1_hitungbahanbakar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,28 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonHitung.setOnClickListener { penghitungan() }
 
+    }
+
+    private fun penghitungan(){
+        val awal = binding.inputJarakAwal.text.toString()
+        if (TextUtils.isEmpty(awal)){
+            Toast.makeText(this,"Lengkapi data.",Toast.LENGTH_LONG).show()
+            return
+        }
+        val akhir = binding.inputJarakAkhir.text.toString()
+        if (TextUtils.isEmpty(akhir)){
+            Toast.makeText(this,"Lengkapi data.",Toast.LENGTH_LONG).show()
+            return
+        }
+        val jumlah = binding.inputBensin.text.toString()
+        if (TextUtils.isEmpty(jumlah)){
+            Toast.makeText(this,"Lengkapi data.",Toast.LENGTH_LONG).show()
+            return
+        }
+        if (awal.toFloat() > akhir.toFloat())
+            Toast.makeText(this,"Jarak tidak valid",Toast.LENGTH_LONG).show()
+            return
     }
 }
